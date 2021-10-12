f2aaad2bf125b4f196c011a0a3707624f9c815b0
Ali-Elshorbagy <ali_elshorbagy@mabrains.com>
2021-10-12 11:15:15 +0200

    Try to add .data inside Xyce netlist

--- testcases/dc/comparator.py
+++ testcases/dc/comparator.py
@@ -10,26 +10,75 @@ def extract_device_dimensions(device_path):
     return dimensions
 
 
-def generate_xyce_netlist(device_name, device_path):
-    dimensions = extract_device_dimensions(device_path)
-    for dimension in dimensions:
-        netlist = f"""
-*Xyce Common Source Circuit
-.lib "../../Models/libs.tech/Xyce/sky130.lib.spice" tt
+def generate_xyce_netlist(device_name: str, corner: str, dimensions: str, supply: float):
+
+    file_name = f"{device_name}_{corner}_xyce"
+
+    netlist = f"""*Xyce Common Source Circuit
+.lib "../../Models/libs.tech/Xyce/sky130.lib.spice" {corner}
+
+***************
+** Parameters
+***************
+.param width=1
+.param length=1
+
+*****************
+** main netlist
+*****************
 X1 d g 0 0 {device_name} {dimension}
 R1 d vdd 1000
 
 V1 g 0 1
-VDD vdd 0 1.8
+VDD vdd 0 {supply}
 
-
-.DC v1 0 1.8 0.1
-.print DC {{-i(vdd)}}
+*****************
+** Analysic
+*****************
+.DC v1 0 {supply} 0.1
+.print DC FILE=./{file_name}.csv FORMAT=CSV {{-i(vdd)}}
 
 .end
-
-
 """
+    with open(f"{file_name}.net", "w") as f:
+        f.write(netlist)
+
+
+def generate_ngspice_netlist(device_name: str, corner: str, dimension: str, supply: float):
+    width = dimension.split()[2]
+    length = dimension.split()[-1]
+    file_name = f"{device_name}_{corner}_w_{width}_l_{length}_xyce"
+
+    netlist = f"""*Ngspice Common Source Circuit
+.lib "../../Models/libs.tech/Xyce/sky130.lib.spice" {corner}
+X1 d g 0 0 {device_name} {dimension}
+R1 d vdd 1000
+
+V1 g 0 1
+VDD vdd 0 {supply}
+
+
+.DC v1 0 {supply} 0.1
+.print DC FILE=./{file_name}.csv FORMAT=CSV {{-i(vdd)}}
+
+.end
+"""
+
+    with open(f"{file_name}.net", "w") as f:
+        f.write(netlist)
+
+
+def simulate(devices_dic):
+    corners = ["tt", "ff", "fs", "sf", "ss"]
+    for device in devices_dic.keys():
+        for corner in corners:
+            dimensions = extract_device_dimensions(devices_dic[device]["path"])
+            generate_xyce_netlist(
+                device_name=device, corner=corner, dimensions=dimensions,
+                supply=devices_dic[device]["supply"])
+
+            break
+        break
 
 
 def main():
@@ -43,9 +92,7 @@ def main():
                     "sky130_fd_pr__pfet_01v8_lvt": {"supply": 1.8, "path": "../../Models/libs.ref/sky130_fd_pr/spice/sky130_fd_pr__pfet_01v8_lvt__tt.corner.spice"},
                     "sky130_fd_pr__pfet_01v8_hvt": {"supply": 1.8, "path": "../../Models/libs.ref/sky130_fd_pr/spice/sky130_fd_pr__pfet_01v8_hvt__tt.corner.spice"}, }
 
-    for nmos_device in nmos_devices.keys():
-        generate_xyce_netlist(nmos_device, nmos_devices[nmos_device]["path"])
-        break
+    simulate(nmos_devices)
 
 
 if __name__ == "__main__":

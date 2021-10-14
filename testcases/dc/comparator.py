f3e2d97fa543d2845784421c00590d4a728869f5
Ali-Elshorbagy <ali_elshorbagy@mabrains.com>
2021-10-14 19:16:51 +0200

    Add comparison part in comparator script. We just need time to simulate all those cases.

--- testcases/dc/comparator.py
+++ testcases/dc/comparator.py
@@ -2,6 +2,7 @@ import re
 import os
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 
 def extract_device_dimensions(device_path):
@@ -21,43 +22,27 @@ def extract_device_dimensions(device_path):
     return dimensions
 
 
-def generate_xyce_netlist(device_name: str, corner: str, dimensions: str, supply: float, file_name: str):
+def generate_xyce_netlist(device_name: str, corner: str, width: str, length: str, supply: float, file_name: str):
     file_name = file_name + "_xyce"
-    dimensions_data = [
-        f"+ {dimension.split()[2]} {dimension.split()[-1]}" for dimension in dimensions]
-    dimensions_data = "\n".join(dimensions_data)
 
     netlist = f"""*Xyce Common Source Circuit
 .lib "../../Models/libs.tech/Xyce/sky130.lib.spice" {corner}
 
-***************
-** Parameters
-***************
-.param width=1.68
-.param length=0.15
-
 *****************
 ** main netlist
 *****************
-X1 d g 0 0 {device_name} W=width L=length
+X1 d g 0 0 {device_name} W={width} L={length}
 R1 d vdd 1000
 
 V1 g 0 1
 VDD vdd 0 {supply}
 
-********
-** data
-********
-.data device_data
-+ width length
-{dimensions_data}
-.enddata
+
 
 *****************
 ** Analysic
 *****************
 .DC v1 0 {supply} 0.1
-.STEP DATA=device_data
 .print DC FILE=./xyce_results/{file_name}.csv FORMAT=CSV {{-i(vdd)}}
 
 .end
@@ -66,21 +51,15 @@ VDD vdd 0 {supply}
         f.write(netlist)
 
 
-def generate_ngspice_netlist(device_name: str, corner: str, dimensions: str, supply: float, file_name: str):
+def generate_ngspice_netlist(device_name: str, corner: str, width: str, length: str, supply: float, file_name: str):
     file_name = file_name + "_ngspice"
-    width_data = [dimension.split()[2] for dimension in dimensions]
-    width_data = " ".join(width_data)
-
-    length_data = [dimension.split()[-1] for dimension in dimensions]
-    length_data = " ".join(length_data)
-
     netlist = f"""*Ngspice Common Source Circuit
 .lib "../../Models/libs.tech/ngspice/sky130.lib.spice" {corner}
 
 *****************
 ** main netlist
 *****************
-X1 d g 0 0 {device_name} W=1.68 L=.15
+X1 d g 0 0 {device_name} W={width} L={length}
 R1 d vdd 1000
 
 V1 g 0 1
@@ -92,23 +71,12 @@ VDD vdd 0 {supply}
 *****************
 .control
 set appendwrite
-compose width_vector  values {width_data}
-compose length_vector values {length_data}
 
 shell  rm -rf ./ngspice_results/{file_name}.csv
 
-let i = 0
-while i < length(width_vector)
-    reset
-    alter @m.x1.m{device_name}[L] = length_vector[i]
-    alter @m.x1.m{device_name}[W] = width_vector[i]
-    
-    DC v1 0 {supply} 0.1
-    wrdata ./ngspice_results/{file_name}.csv  -I(vdd)
-    
-    
-    let i = i + 1
-end
+DC v1 0 {supply} 0.1
+wrdata ./ngspice_results/{file_name}.csv  -I(vdd)
+
 .endc
 .end
 """
@@ -117,42 +85,59 @@ end
 
 
 def compare(devices_dic, corners):
+    comparison_table = f"Device,Width,Length,Corner,Max_error(%)\n"
     for device in devices_dic.keys():
         for corner in corners:
-            base_name = f"{device}_{corner}"
-            xyce_file_name = f"./xyce_results/{base_name}_xyce.csv"
-            ngspice_file_name = f"./ngspice_results/{base_name}_ngspice.csv"
+            dimensions = extract_device_dimensions(devices_dic[device]["path"])
+            for dimension in dimensions:
+                width = dimension.split()[2]
+                length = dimension.split()[-1]
+                base_name = f"{device}_W_{width}_L_{length}_{corner}"
 
-            xyce_df = pd.read_csv(xyce_file_name)
-            ngspice_df = pd.read_csv(ngspice_file_name)
+                xyce_file_name = f"./xyce_results/{base_name}_xyce.csv"
+                ngspice_file_name = f"./ngspice_results/{base_name}_ngspice.csv"
 
-            print(xyce_df[0][0])
+                xyce_df = pd.read_csv(xyce_file_name)
+                ngspice_df = pd.read_csv(ngspice_file_name, header=None)
 
+                xyce_result = np.array(list(xyce_df.iloc[:, 0]))
+                ngspice_result = np.array(list(ngspice_df.iloc[:, 1]))
+
+                error_percentage = np.abs(
+                    (xyce_result-ngspice_result)/xyce_result)*100
+                comparison_table += f"{device},{width},{length},{corner},{np.max(error_percentage):.3f}%\n"
+                break
             break
         break
+    with open("comparison_result.csv", "w")as f:
+        f.write(comparison_table)
 
 
 def simulate(devices_dic, corners):
 
     for device in devices_dic.keys():
         for corner in corners:
-            file_name = f"{device}_{corner}"
             dimensions = extract_device_dimensions(devices_dic[device]["path"])
+            for dimension in dimensions:
+                width = dimension.split()[2]
+                length = dimension.split()[-1]
+                file_name = f"{device}_W_{width}_L_{length}_{corner}"
 
-            Path("./xyce_results").mkdir(parents=True, exist_ok=True)
-            Path("./ngspice_results").mkdir(parents=True, exist_ok=True)
+                Path("./xyce_results").mkdir(parents=True, exist_ok=True)
+                Path("./ngspice_results").mkdir(parents=True, exist_ok=True)
 
-            generate_xyce_netlist(device_name=device, corner=corner, dimensions=dimensions,
-                                  supply=devices_dic[device]["supply"], file_name=file_name)
+                generate_xyce_netlist(device_name=device, corner=corner, width=width, length=length,
+                                      supply=devices_dic[device]["supply"], file_name=file_name)
 
-            generate_ngspice_netlist(device_name=device, corner=corner, dimensions=dimensions,
-                                     supply=devices_dic[device]["supply"], file_name=file_name)
+                generate_ngspice_netlist(device_name=device, corner=corner, width=width, length=length,
+                                         supply=devices_dic[device]["supply"], file_name=file_name)
 
-            # os.system(f"Xyce ./xyce_results/{file_name}_xyce.net")
-            # os.system(f"ngspice -b ./ngspice_results/{file_name}_ngspice.net")
-            # os.system(
-            #     f"sed  -i 's/  /,/g;s/ -/,-/g' ./ngspice_results/{file_name}_ngspice.csv ")
-
+                os.system(f"Xyce ./xyce_results/{file_name}_xyce.net")
+                os.system(
+                    f"ngspice -b ./ngspice_results/{file_name}_ngspice.net")
+                os.system(
+                    f"sed  -i 's/  /,/g;s/ -/,-/g' ./ngspice_results/{file_name}_ngspice.csv ")
+                break
             break
         break
 
